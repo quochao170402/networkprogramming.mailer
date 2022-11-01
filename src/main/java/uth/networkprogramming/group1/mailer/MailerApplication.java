f6ff99ea087db82d50273c20de699fe1530c92c9
quochao170402 <quochao170402@gmail.com>
2022-11-01 23:09:28 +0700

    init project

---
+++ src/main/java/uth/networkprogramming/group1/mailer/MailerApplication.java
@@ -0,0 +1,13 @@
+package uth.networkprogramming.group1.mailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uth/networkprogramming/group1/mailer/MailerApplicationTests.java
@@ -0,0 +1,13 @@
+package uth.networkprogramming.group1.mailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
